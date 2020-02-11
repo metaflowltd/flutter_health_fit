64bd6cf859b0851daea1da6fc5f04261141dd1a5
Avi Mastov <avim82@gmail.com>
2020-02-11 11:13:11 +0200

    Fixed steps counter in a new function "getSteps", returning a typed map.

--- android/src/main/kotlin/com/metaflow/flutterhealthfit/FlutterHealthFitPlugin.kt
+++ android/src/main/kotlin/com/metaflow/flutterhealthfit/FlutterHealthFitPlugin.kt
@@ -1,22 +1,23 @@
 package com.metaflow.flutterhealthfit
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
+import com.google.android.gms.fitness.data.DataSource
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.android.gms.fitness.result.DataReadResponse
 import com.google.android.gms.tasks.Tasks
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.text.DateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -30,11 +31,11 @@ class FlutterHealthFitPlugin(private val activity: Activity) : MethodCallHandler
         val dataType: DataType = DataType.TYPE_STEP_COUNT_DELTA
         val aggregatedDataType: DataType = DataType.AGGREGATE_STEP_COUNT_DELTA
 
-        val TAG = FlutterHealthFitPlugin::class.java.simpleName
+        val TAG: String = FlutterHealthFitPlugin::class.java.simpleName
 
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            if (registrar.activity() == null) return;
+            if (registrar.activity() == null) return
 
             val plugin = FlutterHealthFitPlugin(registrar.activity())
             registrar.addActivityResultListener(plugin)
@@ -53,20 +54,20 @@ class FlutterHealthFitPlugin(private val activity: Activity) : MethodCallHandler
 
             "requestAuthorization" -> connect(result)
 
+            "isAuthorized" -> result.success(isAuthorized())
+
             "getBasicHealthData" -> result.success(HashMap<String, String>())
 
             "getActivity" -> {
-                val name = call.argument<String>("name")
+                val map = HashMap<String, Double>()
+                map["value"] = 0.0
+                result.success(map)
+            }
 
-                when (name) {
-                    "steps" -> getYesterdaysStepsTotal(result)
-
-                    else -> {
-                        val map = HashMap<String, Double>()
-                        map["value"] = 0.0
-                        result.success(map)
-                    }
-                }
+            "getSteps" -> {
+                val start = call.argument<Long>("start")!!
+                val end = call.argument<Long>("end")!!
+                getStepsInRange(start, end, result)
             }
 
             else -> result.notImplemented()
@@ -97,6 +98,11 @@ class FlutterHealthFitPlugin(private val activity: Activity) : MethodCallHandler
         return false
     }
 
+    private fun isAuthorized(): Boolean {
+        val fitnessOptions = getFitnessOptions()
+        return GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity), fitnessOptions)
+    }
+
     private fun connect(result: Result) {
         val fitnessOptions = getFitnessOptions()
 
@@ -124,54 +130,43 @@ class FlutterHealthFitPlugin(private val activity: Activity) : MethodCallHandler
                 }
     }
 
-    private fun getYesterdaysStepsTotal(result: Result) {
+    @SuppressLint("UseSparseArrays") // Dart doesn't know sparse arrays
+    private fun getStepsInRange(start: Long, end: Long, result: Result) {
         val gsa = GoogleSignIn.getAccountForExtension(activity, getFitnessOptions())
 
-        val startCal = GregorianCalendar()
-        startCal.add(Calendar.DAY_OF_YEAR, -1)
-        startCal.set(Calendar.HOUR_OF_DAY, 0)
-        startCal.set(Calendar.MINUTE, 0)
-        startCal.set(Calendar.SECOND, 0)
-
-        val endCal = GregorianCalendar(
-                startCal.get(Calendar.YEAR),
-                startCal.get(Calendar.MONTH),
-                startCal.get(Calendar.DAY_OF_MONTH),
-                23,
-                59)
+        val ds = DataSource.Builder()
+                .setAppPackageName("com.google.android.gms")
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setType(DataSource.TYPE_DERIVED)
+                .setStreamName("estimated_steps")
+                .build()
 
         val request = DataReadRequest.Builder()
-                .aggregate(dataType, aggregatedDataType)
+                .aggregate(ds, DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .bucketByTime(1, TimeUnit.DAYS)
-                .setTimeRange(startCal.timeInMillis, endCal.timeInMillis, TimeUnit.MILLISECONDS)
+                .setTimeRange(start, end, TimeUnit.MILLISECONDS)
                 .build()
 
         val response = Fitness.getHistoryClient(activity, gsa).readData(request)
 
         val dateFormat = DateFormat.getDateInstance()
-        val dayString = dateFormat.format(Date(startCal.timeInMillis))
+        val dayString = dateFormat.format(Date(start))
 
         Thread {
             try {
                 val readDataResult = Tasks.await<DataReadResponse>(response)
                 Log.d(TAG, "buckets count: ${readDataResult.buckets.size}")
 
-                if (!readDataResult.buckets.isEmpty()) {
-                    val dp = readDataResult.buckets[0].dataSets[0].dataPoints[0]
+                val map = HashMap<Long, Int>() // need to return to Dart so can't use sparse array
+                for (bucket in readDataResult.buckets) {
+                    val dp = bucket.dataSets[0].dataPoints[0]
                     val count = dp.getValue(aggregatedDataType.fields[0])
 
                     Log.d(TAG, "returning $count steps for $dayString")
-                    val map = HashMap<String, Double>()
-                    map["value"] = count.asInt().toDouble()
-
-                    activity.runOnUiThread {
-                        result.success(map)
-                    }
-
-                } else {
-                    activity.runOnUiThread {
-                        result.error("No data", "No data found for $dayString", null)
-                    }
+                    map[dp.getStartTime(TimeUnit.MILLISECONDS)] = count.asInt()
+                }
+                activity.runOnUiThread {
+                    result.success(map)
                 }
             } catch (e: Throwable) {
                 Log.e(TAG, "failed: ${e.message}")
@@ -186,5 +181,6 @@ class FlutterHealthFitPlugin(private val activity: Activity) : MethodCallHandler
 
     private fun getFitnessOptions() = FitnessOptions.builder()
             .addDataType(dataType, FitnessOptions.ACCESS_READ)
+            .addDataType(aggregatedDataType, FitnessOptions.ACCESS_READ)
             .build()
 }
