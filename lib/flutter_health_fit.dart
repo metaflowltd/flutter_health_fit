195b4111c4d9eaf8dcd87fc8406603d2dc4a3217
Avi Mastov <avim82@gmail.com>
2020-02-11 22:05:34 +0200

    flutterHealthFit - singleton instead of static. reinstated steps _ActivityType. fixed typo in getCyclingDistance.

--- lib/flutter_health_fit.dart
+++ lib/flutter_health_fit.dart
@@ -3,30 +3,31 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 // Current day's accumulated values
-enum _ActivityType{ cycling, walkRun, heartRate, flights }
+enum _ActivityType { steps, cycling, walkRun, heartRate, flights }
 
 class FlutterHealthFit {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_health_fit');
+  static const MethodChannel _channel = const MethodChannel('flutter_health_fit');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  factory FlutterHealthFit() => _singleton;
+
+  FlutterHealthFit.internal();
+
+  static final _singleton = FlutterHealthFit.internal();
+
+  Future<bool> get isAuthorized async {
+    final status = await _channel.invokeMethod("isAuthorized");
+    return status;
   }
 
-  static Future<bool> get isAuthorized async {
-    return await _channel.invokeMethod("isAuthorized");
-  }
-
-  static Future<bool> authorize() async {
+  Future<bool> authorize() async {
     return await _channel.invokeMethod('requestAuthorization');
   }
 
-  static Future<Map<dynamic, dynamic>> get getBasicHealthData async {
+  Future<Map<dynamic, dynamic>> get getBasicHealthData async {
     return await _channel.invokeMethod('getBasicHealthData');
   }
 
-  static Future<Map<DateTime, int>> getStepsByDay(int start, int end) async {
+  Future<Map<DateTime, int>> getStepsByDay(int start, int end) async {
     Map stepsByTimestamp = await _channel.invokeMethod("getSteps", {"start": start, "end": end});
     return stepsByTimestamp.cast<int, int>().map((int key, int value) {
       var dateTime = DateTime.fromMillisecondsSinceEpoch(key);
@@ -35,39 +36,30 @@ class FlutterHealthFit {
     });
   }
 
-  static Future<double> get getWalkingAndRunningDistance async {
+  Future<double> get getWalkingAndRunningDistance async {
     return await _getActivityData(_ActivityType.walkRun, "m");
   }
 
-  static Future<double> get geCyclingDistance async {
+  Future<double> get getCyclingDistance async {
     return await _getActivityData(_ActivityType.cycling, "m");
   }
 
-  static Future<double> get getFlights async {
+  Future<double> get getFlights async {
     return await _getActivityData(_ActivityType.flights, "count");
   }
 
-  static Future<double> _getActivityData(_ActivityType activityType, String units) async {
+  Future<double> _getActivityData(_ActivityType activityType, String units) async {
     var result;
 
     try {
-      result = await _channel.invokeMethod(
-          'getActivity',
-          {
-            "name": activityType
-                .toString()
-                .split(".")
-                .last,
-            "units": units
-          }
-      );
-    }
-    catch (e) {
+      result =
+          await _channel.invokeMethod('getActivity', {"name": activityType.toString().split(".").last, "units": units});
+    } catch (e) {
       print(e.toString());
       return null;
     }
 
-    if (result == null || result.isEmpty){
+    if (result == null || result.isEmpty) {
       return null;
     }
 
