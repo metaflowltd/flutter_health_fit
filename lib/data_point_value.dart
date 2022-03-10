33496b3bd52adccd7ff52a6a24c5a46fdaa5b7eb
NimGit <neinat2@gmail.com>
2022-03-10 09:18:50 +0200

    LMN-4542 | Source provider value is missing in steps in iOS and Android (#61)
    * ios collect user activity
    
    * collect from ios
    
    * collect from android
    
    Co-authored-by: Nimrod Einat <nimrod@lumen.me>

--- lib/data_point_value.dart
+++ lib/data_point_value.dart
@@ -26,12 +26,12 @@ class DataPointValue {
     final value = map["value"] as double?;
     final units =  DataPointUnitUtils.getUnit(fromString: map["units"] as String?);
     final sourceApp = map["sourceApp"] as String?;
+    final endDateInMillis = map["endDateInMillis"] as int?;
 
-    if (date == null || value == null || units == null) {
+    if (value == null || units == null) {
       return null;
     }
 
-    return DataPointValue(
-        value: value, date: date, units: units, sourceApp: sourceApp);
+    return DataPointValue(value: value, date: date, units: units, sourceApp: sourceApp);
   }
 }
